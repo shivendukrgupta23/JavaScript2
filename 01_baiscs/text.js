47e95c448f66c1b03734fda9f68ff03d8f0faf35
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-07-14 13:28:32 +0000

    datatypes

--- 01_baiscs/text.js
+++ 01_baiscs/text.js
@@ -1,1 +1,23 @@
-console.log("Shivendu")
+const accountId= 43353
+let accountEmail= "hitesh@google.com"
+var accountPassword= "12345"
+accountCity= "Jaipur"
+let accountState;
+
+// accountId=2//not allowed
+
+/*
+
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail="hc@hc.com"
+accountPassword= "2122"
+accountCity="Bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])

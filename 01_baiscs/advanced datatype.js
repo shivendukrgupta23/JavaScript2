81e990850b2b5404e10c6ff77c901643096d6cf1
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-07-17 13:21:42 +0530

    Create advanced datatype.js

---
+++ 01_baiscs/advanced datatype.js
@@ -0,0 +1,14 @@
+//Primitive 
+//7types: String ,Number, Boolean, null, undefined, Symbol, BigInt
+const score= 100
+const scoreValue=100.3
+
+const isLoggedIn = False
+const outsideTemp =null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+//Reference(Non-Primitive)-: Array, objects,Functions
+
